e33466fa00c1366780b93b007becb8203000d225
yonatan46 <yonatankefyalew65@gmail.com>
2022-06-15 06:37:33 +0300

    first question

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef __HEADER__
+#define __HEADER__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+int input_string(char *str);
+
+#endif
--- test.c
+++ test.c
@@ -1,12 +1,37 @@
 #include <stdio.h>
+#include "main.h"
 /**
  * main - prints hello world
+ * @argc: number of arguments passed
+ * @argv: the arguments passed
  * Return: 0 on success
  */
 
-int main(void)
+int main(int argc, char **argv)
 {
-	printf("hello world\n");
+	char str[1000];
+	pid_t pid;
 
+	while (1)
+	{
+		printf("$ ");
+		if (input_string(str) == 0)
+		{
+			pid = fork();
+			if (pid == 0)
+			{
+				if (execve(str, argv, NULL) == -1)
+					perror(argv[0]);
+			}
+			else
+				wait(NULL);
+		}
+		else
+		{
+			printf("\n");
+			break;
+		}
+
+	}
 	return (0);
 }
